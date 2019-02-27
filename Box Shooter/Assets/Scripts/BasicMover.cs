2cbcf03884553e96d3be176ceb90bcb557c92be8
ritz00000000 <ritz00000000@hotmail.com>
2019-02-27 12:54:37 -0500

    Box Shooter

---
+++ Box Shooter/Assets/Scripts/BasicMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour {
+
+    public float spinSpeed=180.0f;
+    public float motionMagnitude = 0.1f;
+	
+	// Update is called once per frame
+	void Update () {
+        //rotate around the up axis of the gameObject
+        gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+
+        //move up and down over time 
+	}
+}
